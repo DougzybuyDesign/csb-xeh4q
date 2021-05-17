23e2eb46b5d48c89449339f427b556b5d61ec0b3
Code Sandbox <hello@codesandbox.io>
2021-05-17 11:13:30 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.91ed51d2e8b9e0d4060866e6e3a66448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e504bb9445425c032a7",
+    "url": "https://dougzybuydesign.github.io/csb-xeh4q/static/css/main.92251c7c.chunk.css"
+  },
+  {
+    "revision": "7e504bb9445425c032a7",
+    "url": "https://dougzybuydesign.github.io/csb-xeh4q/static/js/main.7e504bb9.chunk.js"
+  },
+  {
+    "revision": "eddf9f65d2f0d34618af",
+    "url": "https://dougzybuydesign.github.io/csb-xeh4q/static/js/1.eddf9f65.chunk.js"
+  },
+  {
+    "revision": "b2800f99dfa14603c0c8",
+    "url": "https://dougzybuydesign.github.io/csb-xeh4q/static/js/runtime~main.b2800f99.js"
+  },
+  {
+    "revision": "a58f7626fd9461adea3de360d2da06ec",
+    "url": "https://dougzybuydesign.github.io/csb-xeh4q/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://dougzybuydesign.github.io/csb-xeh4q/precache-manifest.91ed51d2e8b9e0d4060866e6e3a66448.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://dougzybuydesign.github.io/csb-xeh4q/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
